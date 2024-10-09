de1756868498c86b44a63c66ad20319f2266e0b2
Roney Benyhe Rodrigues <roneybenyhebrs@gmail.com>
2024-10-09 13:59:02 -0300

    create entities

--- src/modules/cars/typeorm/entities/Car.ts
+++ src/modules/cars/typeorm/entities/Car.ts
@@ -1,5 +1,42 @@
-import { entity } from 'typeorm'
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Item } from './Items';
 
-@entity('cars'){
-    
+@Entity('cars')
+export class Cars {
+	@PrimaryGeneratedColumn('uuid')
+	id!: string;
+
+	@Column('varchar')
+	plate!: string;
+
+	@Column('varchar')
+	brand!: string;
+
+	@Column('varchar')
+	model!: string;
+
+	@Column('decimal')
+	km!: number;
+
+	@Column('int')
+	year!: number;
+
+	@Column({
+		type: 'enum',
+		enum: ['ativo', 'inativo', 'excluído'],
+		default: 'ativo',
+	})
+	status!: string;
+
+	@Column('timestamp')
+	created_at!: Date;
+
+	@Column('timestamp')
+	updated_at!: Date;
+
+	@OneToMany(
+		() => Item,
+		(item) => item.car,
+	)
+	items!: Item[];
 }
