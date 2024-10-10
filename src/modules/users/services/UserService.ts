de0f7f2ec9765f13062e12899bcaf8a51c43b472
Álvaro Silva Garcia <109820488+Alvarosig@users.noreply.github.com>
2024-10-10 13:20:00 -0300

    fix/refact: resolve conflicts

--- src/modules/users/services/UserService.ts
+++ src/modules/users/services/UserService.ts
@@ -1,6 +1,9 @@
 import ConflictError from '@/http/errors/conflict-error';
 import bcrypt from 'bcryptjs';
-import type { CreateUserInterface } from '../typeorm/entities/interfaces/UserInterface';
+import type {
+	CreateUserInterface,
+	UserDetailsInterface,
+} from '../typeorm/entities/interfaces/UserInterface';
 import type UserRepositoryInterface from '../typeorm/repositories/interfaces/UserRepositoryInterface';
 import type UserServiceInterface from './interfaces/UserServiceInterface';
 import UserRepository from '../typeorm/repositories/UserRepository';
@@ -25,5 +28,9 @@ export default class UserService implements UserServiceInterface {
 
 	async findActiveUserByEmail(email: string) {
 		return await this.repository.findActiveUserByEmail(email);
+	}
+
+	async findById(id: string): Promise<UserDetailsInterface | null> {
+		return await this.repository.findById(id);
 	}
 }
