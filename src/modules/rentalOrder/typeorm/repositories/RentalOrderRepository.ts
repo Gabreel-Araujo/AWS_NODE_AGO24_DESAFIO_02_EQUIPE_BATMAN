9d9869ab2eae7aeccb55ef4f49e41420584e2afb
Gabriel Alves <87396370+Gabreel-Araujo@users.noreply.github.com>
2024-10-13 21:13:51 -0300

    feat: add method to fetch only active cars in CarsRepository

--- src/modules/rentalOrder/typeorm/repositories/RentalOrderRepository.ts
+++ src/modules/rentalOrder/typeorm/repositories/RentalOrderRepository.ts
@@ -1,6 +1,9 @@
 import { dbConnection } from '@/lib/typeorm';
 import { In, Repository } from 'typeorm';
-import { ICreateRentalOrder, IRentalOrder } from '../entities/interfaces/RentalOrderInterface';
+import {
+	ICreateRentalOrder,
+	IRentalOrder,
+} from '../entities/interfaces/RentalOrderInterface';
 import RentalOrder from '../entities/RentalOrder';
 import { IRentalOrderRepository } from './interfaces/IRentalOrderRepository';
 
@@ -10,29 +13,41 @@ class RentalOrderRepository implements IRentalOrderRepository {
 	constructor() {
 		this.ormRepository = dbConnection.getRepository(RentalOrder);
 	}
-    public async findByCustomer(customer_id: string): Promise<RentalOrder | null> {
-        const customer = await this.ormRepository.findOne({
-			where: {customer_id , status: In(['open'])},
+	public async findByCustomer(
+		customer_id: string,
+	): Promise<RentalOrder | null> {
+		const customer = await this.ormRepository.findOne({
+			where: { customer_id, status: In(['open']) },
 		});
 		return customer;
-    }
-    public async findByCar(car_id: string): Promise<RentalOrder | null> {
-        const car = await this.ormRepository.findOne({
-			where: {car_id , status: In(['open', 'aproved'])},
+	}
+	public async findByCar(car_id: string): Promise<RentalOrder | null> {
+		const car = await this.ormRepository.findOne({
+			where: { car_id, status: In(['open', 'aproved']) },
 		});
 		return car;
-    }
-    public async findById(id: string): Promise<RentalOrder | null> {
-        const rentalOrder = await this.ormRepository.findOne({
-			where: {id},
+	}
+	public async findById(id: string): Promise<RentalOrder | null> {
+		const rentalOrder = await this.ormRepository.findOne({
+			where: { id },
 		});
 		return rentalOrder;
-    }
+	}
 	public async save(order: ICreateRentalOrder): Promise<RentalOrder> {
-        const rentalOrder = this.ormRepository.create(order);
-        await this.ormRepository.save(rentalOrder);
-        return rentalOrder;
-    }    
+		const rentalOrder = this.ormRepository.create(order);
+		await this.ormRepository.save(rentalOrder);
+		return rentalOrder;
+	}
+
+	public async findActiveOrdersByCarId(carId: string): Promise<boolean> {
+		const activeOrders = await this.ormRepository.find({
+			where: {
+				car_id: carId,
+				status: In(['open', 'aproved']),
+			},
+		});
+		return activeOrders.length > 0;
+	}
 }
 
 export default RentalOrderRepository;
